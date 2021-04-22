89b0fc8b0a8bfde1695c606d4933176aaab1229b
Ubuntu <ubuntu@ip-172-31-41-190.us-west-2.compute.internal>
2021-04-22 04:40:20 +0000

    Add migrations for production

---
+++ db/migrate/20210422043053_article.rb
@@ -0,0 +1,9 @@
+class Article < ActiveRecord::Migration[6.1]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :description
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_051908) do
+ActiveRecord::Schema.define(version: 2021_04_22_043053) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
