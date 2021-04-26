b9d30e6d66a377c39b56c2715fe2719968928f88
Ubuntu <ubuntu@ip-172-31-41-190.us-west-2.compute.internal>
2021-04-26 03:08:18 +0000

    Add edit template and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  
   def new
     @user = User.new
   end 
@@ -15,7 +18,31 @@ class UsersController < ApplicationController
     end
   end 
   
+  def edit
+  end 
+  
+  def show
+  end 
+  
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Your profile details were successfully updated."
+      redirect_to articles_path
+    else 
+      render 'edit'    
+    end 
+  end 
+  
+  def destroy
+  end 
+  
   private 
+  
+  
+  def set_user
+    @user = User.find(params[:id])
+  end 
+  
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end 
