b8424040f59d25754d0465d4e430042fc2108ab4
Ubuntu <ubuntu@ip-172-31-41-190.us-west-2.compute.internal>
2021-04-26 03:31:33 +0000

    Add user show page with article listing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,7 @@ class UsersController < ApplicationController
   end 
   
   def show
+    @articles = @user.articles
   end 
   
   def update
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  
+  def gravatar_for(user, options = { size: 80 })
+    size = options[:size]
+    image_url = "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}?s=#{size}"
+    image_tag(image_url, alt:user.username, class: "rounded shadow mx-auto d-block")
+  end
+  
 end
